4f5f5320cf05a72518a35412063470a9731d7e3d
TissueRoll <christopher.dizon@protonmail.com>
2019-09-16 18:06:32 +0800

    got the x,y positions of each hex tile (displayed as <y,x,z>)

---
+++ RecoveReef Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool lb = Input.GetMouseButtonDown(0);
+        if (lb) {
+            print("left mouse button has been pressed");
+            Grid grid = GameObject.Find("Grid").GetComponent<Grid>();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldPoint = ray.GetPoint(-ray.origin.z/ray.direction.z);
+            Vector3Int position = grid.WorldToCell(worldPoint);
+            print("position: " + position);
+        }
+    }
+}
