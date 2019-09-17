a73feb587d3a8bb0a010460584d06e00ee6b4539
TissueRoll <christopher.dizon@protonmail.com>
2019-09-17 23:07:26 +0800

    camera movement and deleting tiles done

--- RecoveReef Game/Assets/Scripts/GameManager.cs
+++ RecoveReef Game/Assets/Scripts/GameManager.cs
@@ -1,11 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
-    // Start is called before the first frame update
+
+    #pragma warning disable 0649
+    [SerializeField] private CameraFollow cameraFollow;
+    [SerializeField] private Tilemap tileMap;
+    #pragma warning restore 0649
+    private float zoom = 10f;
+    private Vector3 cameraFollowPosition;
+    private bool edgeScrollingEnabled = false;
+    private Dictionary<Vector3,string> cells;
+
+    private void Start() {
+        // sets the cameraFollowPosition to the default 
+        cameraFollowPosition = cameraFollow.transform.position;
+        cameraFollow.Setup(() => cameraFollowPosition, () => zoom);
+        // cameraFollow.Setup(() => new Vector3(0,-100));
+    }
     void Awake()
     {
         if (instance == null) {
@@ -18,6 +35,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // testing for hex tile coords
         bool lb = Input.GetMouseButtonDown(0);
         if (lb) {
             print("left mouse button has been pressed");
@@ -26,6 +44,68 @@ public class GameManager : MonoBehaviour
             Vector3 worldPoint = ray.GetPoint(-ray.origin.z/ray.direction.z);
             Vector3Int position = grid.WorldToCell(worldPoint);
             print("position: " + position);
+            tileMap.SetTile(position, null);
+            
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            edgeScrollingEnabled = !edgeScrollingEnabled;
+            print("edgeScrolling = " + edgeScrollingEnabled);
+        }
+
+        // movement of screen
+        moveCameraWASD(20f);
+        if (edgeScrollingEnabled) moveCameraMouseEdge(20f,10f);
+        zoomKeys(1f);
+    }
+
+    private void moveCameraWASD(float moveAmount) {
+        if (Input.GetKey(KeyCode.W)) {
+            cameraFollowPosition.y += moveAmount * Time.deltaTime;
+        } 
+        if (Input.GetKey(KeyCode.A)) {
+            cameraFollowPosition.x -= moveAmount * Time.deltaTime;
+        } 
+        if (Input.GetKey(KeyCode.S)) {
+            cameraFollowPosition.y -= moveAmount * Time.deltaTime;
+        } 
+        if (Input.GetKey(KeyCode.D)) {
+            cameraFollowPosition.x += moveAmount * Time.deltaTime;
         }
     }
+
+    private void moveCameraMouseEdge(float moveAmount, float edgeSize) {
+        if (Input.mousePosition.x > Screen.width - edgeSize) {
+            cameraFollowPosition.x += moveAmount * Time.deltaTime;
+        }
+        if (Input.mousePosition.x < edgeSize) {
+            cameraFollowPosition.x -= moveAmount * Time.deltaTime;
+        }
+        if (Input.mousePosition.y > Screen.height - edgeSize) {
+            cameraFollowPosition.y += moveAmount * Time.deltaTime;
+        }
+        if (Input.mousePosition.y < edgeSize) {
+            cameraFollowPosition.y -= moveAmount * Time.deltaTime;
+        }
+    }
+
+    private void zoomKeys(float zoomChangeAmount) {
+        if (Input.GetKey(KeyCode.KeypadPlus)) {
+            zoom -= zoomChangeAmount * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.KeypadMinus)) {
+            zoom += zoomChangeAmount * Time.deltaTime;
+        }
+
+        if (Input.mouseScrollDelta.y > 0) {
+            zoom -= zoomChangeAmount;
+        }
+        if (Input.mouseScrollDelta.y < 0) {
+            zoom += zoomChangeAmount;
+        }
+
+        zoom = Mathf.Clamp(zoom, 5f, 15f);
+    }
+
 }
