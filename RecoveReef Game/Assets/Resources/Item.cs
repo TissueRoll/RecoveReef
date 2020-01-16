36f3914605c035aa4dbc5a7e7a42975cf9f45d26
TissueRoll <christopher.dizon@protonmail.com>
2020-01-16 22:01:37 +0800

    added test scene and example stuff from tutorial for xml stuff

---
+++ RecoveReef Game/Assets/Resources/Item.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class Item
+{
+    [XmlAttribute("name")]
+    public string name;
+
+    [XmlAttribute("Damage")]
+    public string damage;
+    
+    [XmlAttribute("Durability")]
+    public string durability;
+}
---
+++ RecoveReef Game/Assets/Resources/ItemContainer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+[XmlRoot("ItemCollection")]
+public class ItemContainer
+{
+
+    [XmlArray("Items")]
+    [XmlArrayItem("Item")]
+    public List<Item> items = new List<Item>();
+
+    public static ItemContainer Load(string path) {
+        TextAsset ta = Resources.Load<TextAsset>(path);
+        XmlSerializer serializer = new XmlSerializer(typeof(ItemContainer));
+        StringReader reader = new StringReader(ta.text);
+        ItemContainer items = serializer.Deserialize(reader) as ItemContainer;
+        reader.Close();
+        return items;
+    }
+}
