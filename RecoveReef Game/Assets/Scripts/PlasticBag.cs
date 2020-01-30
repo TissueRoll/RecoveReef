b22987144d23b9f118f9117d26b5ea0a9aff9559
TissueRoll <christopher.dizon@protonmail.com>
2020-01-30 15:23:35 +0800

    added small stuff for demonstration

--- RecoveReef Game/Assets/Scripts/PlasticBag.cs
+++ RecoveReef Game/Assets/Scripts/PlasticBag.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlasticBag : MonoBehaviour
 {
     void OnMouseDown() {
-        LeanTween.scale(this.gameObject, new Vector3(2,2,0), 0.3f);
+        LeanTween.scale(this.gameObject, new Vector3(3,3,0), 1.0f);
+        LeanTween.scale(this.gameObject, new Vector3(1,1,0), 1.0f);
         Destroy(this.gameObject, 0.5f);
     }
 }
