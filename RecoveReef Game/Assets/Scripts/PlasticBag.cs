62a5cf8f22a41d3f843e56a5a888c3a7a6ae73c4
TissueRoll <christopher.dizon@protonmail.com>
2020-02-06 22:23:25 +0800

    various UI fixes and fixed plastic bag mouse down bug

--- RecoveReef Game/Assets/Scripts/PlasticBag.cs
+++ RecoveReef Game/Assets/Scripts/PlasticBag.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlasticBag : MonoBehaviour
 {
+    private bool hasBeenClicked = false;
     void Start() {
         Vector2 randomPositionOnScreen = Camera.main.ViewportToWorldPoint(new Vector2(UnityEngine.Random.value, UnityEngine.Random.value));
         Vector3 pos = new Vector3(randomPositionOnScreen.x, randomPositionOnScreen.y+10, 0);
@@ -14,6 +15,9 @@ public class PlasticBag : MonoBehaviour
         }
     }
     void OnMouseDown() {
+        if (hasBeenClicked)
+            return;
+        hasBeenClicked = true;
         GameManager.instance.adjustTotalPlasticBags(-1);
         Vector3 tmp = this.gameObject.GetComponent<RectTransform>().localScale;
         LeanTween.scale(this.gameObject, tmp*2, 0.3f).setLoopPingPong(1);
