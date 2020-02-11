29108c3d5f70c8e215a66ac0dea92056056f7968
TissueRoll <christopher.dizon@protonmail.com>
2020-02-12 01:42:41 +0800

    attempted hover text but disabled it for now

---
+++ RecoveReef Game/Assets/Scripts/MouseInOut.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseInOut : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private bool mouseEntered = false;
+
+    void Start() {
+        HoverTextCS.hideHoverStatic();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        mouseEntered = true;
+        HoverTextCS.showHoverStatic("blah");
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        mouseEntered = false;
+        HoverTextCS.hideHoverStatic();
+    }
+}
